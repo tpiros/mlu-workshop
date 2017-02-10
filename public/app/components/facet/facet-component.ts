e338614a30ccdf788168bab1d9b049283e84a6a4
Tamas Piros <me@tamas.io>
2017-02-10 17:43:02 +0100

    facet

---
+++ public/app/components/facet/facet-component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[sw-facet]',
+  templateUrl: '/app/components/facet/facet.html'
+})
+export class FacetComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
